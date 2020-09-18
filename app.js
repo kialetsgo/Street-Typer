aba7ac2846a6f92fd8f8e9015720717f6241c956
raysonckl <chewkialeng@gmail.com>
2020-09-19 02:21:48 +0800

    added audio and high score reset

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 // words are randomized every time game is restarted
 // word display list will only show maximum 5 words
 // word display list will "scroll" everytime a word is typed
-// End game condition: given 30 seconds, finish typing all words given or when time = 0
+// End game condition: given 35 seconds, finish typing all words given or when time = 0
 // points given according to word length
 // inputfield will auto clear if inputfield value length =  word length + 1
 // minus 1 point for every wrong input
@@ -84,8 +84,9 @@ let wordCount = 0; // initializing word count for each level
 let level = 0; // level set to 0 as default (easy mode)
 let score = 0;
 let highScore = 0;
-let countdown = 30000; // time given to play the game
+let countdown = 35000; // time given to play the game
 let isPlaying = false; // game play
+let retrievedScores = localStorage.getItem('highScore');
 
 // dom
 let inputEl = document.querySelector('#word-input');
@@ -97,6 +98,7 @@ let currentWordEl = document.querySelector('.current-word');
 let ryuIdle = document.querySelector('#ryu-idle');
 let ryuHadoken = document.querySelector('#ryu-hadoken');
 let boss = document.querySelector('#boss');
+let resetButton = document.querySelector('#reset-highscore');
 
 // modal
 let modalGameStartEl = document.querySelector('.modal-gamestart');
@@ -105,6 +107,19 @@ let modalGameOverEl = document.querySelector('.modal-gameover');
 let modalInstructionEl = document.querySelector('.modal-instructions');
 let modalScoreEl = document.querySelector('#modal-score');
 let modalHighScoreEl = document.querySelector('#modal-highscore');
+
+//sounds
+let hadoukenSound = document.querySelector('#hadouken-sound');
+let punchSound = document.querySelector('#punch-sound');
+let chooseSound = document.querySelector('#choose-sound');
+let startGameSound = document.querySelector('#startgame-sound');
+let fightSound = document.querySelector('#fight-sound');
+let selectingSound = document.querySelector('#selecting-sound');
+let gameOverSound = document.querySelector('#gameover-sound');
+let ryuStageMusic = document.querySelector('#ryustage-music');
+let guileStageMusic = document.querySelector('#guilestage-music');
+let chooseMusic = document.querySelector('#choose-music');
+let titleMusic = document.querySelector('#title-music');
 
 // buttons
 let easyButton = document.querySelector('#easy');
@@ -162,14 +177,14 @@ function setHighScore() {
     localStorage.setItem('highScore', JSON.stringify(highScore));
   }
   modalHighScoreEl.innerHTML = 'High Score: ' + highScore;
-  // score = 0;
-  // scoreEl.innerHTML = score;
 }
 
 function displayHighScore() {
-  let retrievedScores = localStorage.getItem('highScore');
   // after page refresh, highScore is being set back to 0, need to
   highScore = retrievedScores;
+  // if (highScore == 0) {
+  //   highScoreEl.innerHTML = '';
+  // }
   if (highScoreEl.innerHTML === '') {
     highScoreEl.innerHTML = retrievedScores;
   }
@@ -194,7 +209,7 @@ function generateWordList(difficulty) {
 
 function startTimer() {
   isPlaying = true;
-  countdown = 29000;
+  countdown = 34000;
   setInterval(() => {
     if (countdown != 0) {
       timerEl.innerHTML = countdown / 1000;
@@ -215,39 +230,75 @@ function displayCurrentWord(count) {
   }
 }
 
-function compareWords() {
-  let liEl = document.querySelectorAll('.word');
-  // only run for as long as the array length
-  if (wordCount !== words[level].length) {
-    if (inputEl.value === words[level][wordCount]) {
-      liEl[wordCount].style.color = 'green';
-      liEl[wordCount].innerHTML = '';
-      wordCount += 1;
-      hadokenOn();
-      bossHit();
-      // display next word
-      displayCurrentWord(wordCount);
-      // resetting input field
-      inputEl.value = '';
-      addScore();
-      // update word list with next 5 words
-      displayWordList(wordCount);
-    } else if (
-      inputEl.value.length === words[level][wordCount].length + 1 &&
-      inputEl.value !== words[level][wordCount]
-    ) {
-      inputEl.value = '';
-      deductScore();
-    } else {
-      liEl[wordCount].style.color = 'red';
-    }
-  } else {
-    gameOver();
-  }
+function hadoukenSoundEffect() {
+  hadoukenSound.style.display = 'none';
+  hadoukenSound.play();
 }
-function hadokenOn() {
+
+function punchSoundEffect() {
+  punchSound.style.display = 'none';
+  punchSound.play();
+}
+
+function chooseSoundEffect() {
+  chooseSound.style.display = 'none';
+  chooseSound.play();
+}
+
+function startGameSoundEffect() {
+  startGameSound.style.display = 'none';
+  startGameSound.play();
+}
+
+function fightSoundEffect() {
+  fightSound.style.display = 'none';
+  fightSound.play();
+}
+
+function selectingSoundEffect() {
+  selectingSound.style.display = 'none';
+  selectingSound.play();
+}
+
+function gameOverSoundEffect() {
+  gameOverSound.style.display = 'none';
+  gameOverSound.play();
+}
+
+function playChooseMusic() {
+  chooseMusic.style.display = 'none';
+  chooseMusic.play();
+  chooseMusic.loop = true;
+}
+
+function pauseChooseMusic() {
+  chooseMusic.pause();
+}
+
+function playRyuStageMusic() {
+  ryuStageMusic.style.display = 'none';
+  ryuStageMusic.play();
+  ryuStageMusic.loop = true;
+}
+
+function pauseRyuStageMusic() {
+  ryuStageMusic.pause();
+}
+
+function playTitleMusic() {
+  titleMusic.style.display = 'none';
+  titleMusic.play();
+  titleMusic.muted = false;
+}
+
+function pauseTitleMusic() {
+  titleMusic.pause();
+}
+
+function hadoukenOn() {
   ryuIdle.style.display = 'none';
   ryuHadoken.style.display = 'inline-block';
+  hadoukenSoundEffect();
   setTimeout(() => {
     ryuHadoken.style.display = 'none';
     ryuIdle.style.display = 'inline-block';
@@ -256,6 +307,7 @@ function hadokenOn() {
 function bossHit() {
   setTimeout(() => {
     boss.classList.add('boss-shake');
+    punchSoundEffect();
     setTimeout(() => {
       boss.classList.remove('boss-shake');
     }, 500);
@@ -273,6 +325,8 @@ function gameOver() {
   }
   setHighScore();
   showGameOverModal();
+  gameOverSoundEffect();
+  pauseRyuStageMusic();
 }
 
 function showInstructionModal() {
@@ -300,6 +354,36 @@ function hideGameOverModal() {
   modalGameOverEl.style.display = 'none';
 }
 
+function compareWords() {
+  let liEl = document.querySelectorAll('.word');
+  // only run for as long as the array length
+  if (wordCount !== words[level].length) {
+    if (inputEl.value === words[level][wordCount]) {
+      liEl[wordCount].innerHTML = '';
+      wordCount += 1;
+      hadoukenOn();
+      bossHit();
+      // display next word
+      displayCurrentWord(wordCount);
+      // resetting input field
+      inputEl.value = '';
+      addScore();
+      // update word list with next 5 words
+      displayWordList(wordCount);
+    } else if (
+      inputEl.value.length === words[level][wordCount].length + 1 &&
+      inputEl.value !== words[level][wordCount]
+    ) {
+      inputEl.value = '';
+      deductScore();
+    } else {
+      liEl[wordCount].style.color = 'red';
+    }
+  } else {
+    gameOver();
+  }
+}
+
 // actual deployment
 window.onload = function () {
   hideGameOverModal();
@@ -308,12 +392,24 @@ window.onload = function () {
   displayHighScore();
   timerEl.innerHTML = countdown / 1000;
 
-  // let retrievedScores = localStorage.getItem('highscore')
-  // console.log(retrievedScores)
+  // modalGameStartEl.addEventListener('mouseover', function () {
+  //   playTitleMusic();
+  // });
 
   modalGameStartEl.addEventListener('click', function () {
+    startGameSoundEffect();
     hideGameStartModal();
     showInstructionModal();
+    pauseTitleMusic();
+    playChooseMusic();
+  });
+
+  easyButton.addEventListener('mouseover', function () {
+    selectingSoundEffect();
+  });
+
+  hardButton.addEventListener('mouseover', function () {
+    selectingSoundEffect();
   });
 
   easyButton.addEventListener('click', function () {
@@ -323,7 +419,10 @@ window.onload = function () {
       generateWordList(level);
       displayWordList(wordCount);
       displayCurrentWord(wordCount);
+      chooseSoundEffect();
       hideInstructionModal();
+      pauseChooseMusic();
+      playRyuStageMusic();
     }
   });
 
@@ -334,7 +433,10 @@ window.onload = function () {
       generateWordList(level);
       displayWordList(wordCount);
       displayCurrentWord(wordCount);
+      chooseSoundEffect();
       hideInstructionModal();
+      pauseChooseMusic();
+      playRyuStageMusic();
     }
   });
 
@@ -343,9 +445,16 @@ window.onload = function () {
     hideGameOverModal();
   });
 
+  resetButton.addEventListener('click', function () {
+    localStorage.clear();
+    location.reload();
+    hideGameOverModal();
+  });
+
   inputEl.oninput = function () {
     if (isPlaying != true) {
       startTimer();
+      fightSoundEffect();
     }
     compareWords();
   };
